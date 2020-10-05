52c620efb0ccf7892214d5ece11b8ab163ae8efd
MilladMozhdehi <miladmozhdehipour73@gmail.com>
2020-10-05 16:57:29 +0330

    Updates

---
+++ precache-manifest.d6377ca275e106bb62ad7a4198ca6c37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ddcbe975a7345060966209ffb08e84",
+    "url": "/Fresume/index.html"
+  },
+  {
+    "revision": "fa93a8e7b0ebeceb0ca8",
+    "url": "/Fresume/static/css/main.9806fafd.chunk.css"
+  },
+  {
+    "revision": "e0a92338bfa493810c2e",
+    "url": "/Fresume/static/js/2.de515d66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fresume/static/js/2.de515d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa93a8e7b0ebeceb0ca8",
+    "url": "/Fresume/static/js/main.dea30a2f.chunk.js"
+  },
+  {
+    "revision": "68c857197df709f09d63",
+    "url": "/Fresume/static/js/runtime-main.da82c492.js"
+  }
+]);
